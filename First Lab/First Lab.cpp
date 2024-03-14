755dc6f43cc521d257b41bf53d02ba251951f1f8
Yaroslav <iros04004@gmail.com>
2024-03-15 02:11:13 +0300

    Как будто бы с поразрядным вычитанием не все так просто, надо разбираться

--- First Lab/First Lab.cpp
+++ First Lab/First Lab.cpp
@@ -59,6 +59,33 @@ static void BitWiseAddition() {
 
 	cout << "Bit wise addition result: " << result << endl;
 }
+//static void BitWiseSubtruction() {
+//	string num1, num2;
+//	cout << "Enter first number: ";
+//	cin >> num1;
+//	cout << "\nEnter second number: ";
+//	cin >> num2;
+//	int maxlen = max(num1.length(), num2.length());
+//	num1 = string(maxlen - num1.length(), '0') + num1;
+//	num2 = string(maxlen - num2.length(), '0') + num2;
+//
+//	string result = "";
+//	int borrow = 0;
+//	for (int i = maxlen - 1; i >= 0; i--) {
+//		int diff = (num1[i] - '0') - (num2[i] - '0') - borrow;
+//		if (diff < 0) {
+//			diff += 10;
+//			borrow = 1;
+//		}
+//		else {
+//			borrow = 0;
+//		}
+//		result = to_string(diff) + result;
+//	}
+//	
+//	cout << result;
+//}
+
 static void CubicEquation() {
 	double a, b, c, d;
 	cout << "Enter a: \n";
@@ -247,12 +274,15 @@ static void ActionScene() {
 	case 7:
 		//Поразрядные операции с числами введенными ввиде строк
 		BitWiseAddition();
+		/*BitWiseSubtruction();*/
 
 
 	}
 }
 int main() {
 	ActionScene();
+	
+	
 	
 }
 
