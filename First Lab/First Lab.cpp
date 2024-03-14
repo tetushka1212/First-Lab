a9f6cfddf74b17e6bd5bca50e3928673abd5b251
Yaroslav <iros04004@gmail.com>
2024-03-14 23:32:43 +0300

    Подправил вывод, испраивл костыли в шестом кейсе

--- First Lab/First Lab.cpp
+++ First Lab/First Lab.cpp
@@ -35,53 +35,59 @@ static int ActionChooser() {
 	if (case_num > 0 && case_num < 8) { return case_num; }
 	else { cout << "Error.Try again \n"; return -1; }
 }
-void cube_sol() {
+static void CubicEquation() {
 	double a, b, c, d;
-	cin >> a>> b>> c>> d;
-	complex <double>x1, x2, x3;
-	double p;
-	double q;
-	double disk;
-	double fi;
-	double disk2;
-	p = (3.0 * a * c - b * b) / (3.0 * a * a);
-	q = (-9.0 * a * b * c + 27.0 * a * a * d + 2.0 * b * b * b) / (27.0 * a * a * a);
-	disk = (p / 3.0) * (p / 3.0) * (p / 3.0) + (q / 2.0) * (q / 2.0);
-	cout << disk << "  after enterence" << endl;
-	disk2 = disk;
-	if (disk < 0) {
-		cout << disk << "3 real" << endl;
-		if (q < 0) { fi = atan(sqrt(disk) / (-q / 2.0)); }
-		else if (q == 0) { fi = pi / 2.0; }
-		else { fi = (atan(sqrt(-disk) / (-q / 2.0))) + pi; }
+	cout << "Enter a: \n";
+	cin >> a;
+	cout << "Enter b: \n"; 
+	cin >> b;
+	cout << "Enter c: \n"; 
+	cin >> c;
+	cout << "Enter d: \n"; 
+	cin>> d;
+	if (a!=0){
+		complex <double>x1, x2, x3;
+		double p;
+		double q;
+		double disk;
+		double fi;
+		p = (3.0 * a * c - b * b) / (3.0 * a * a);
+		q = (-9.0 * a * b * c + 27.0 * a * a * d + 2.0 * b * b * b) / (27.0 * a * a * a);
+		disk = (p / 3.0) * (p / 3.0) * (p / 3.0) + (q / 2.0) * (q / 2.0);
+		if (disk < 0) {
+			cout <<"Three real solutions: " << endl;
+			if (q < 0) { fi = atan(sqrt(disk) / (-q / 2.0)); }
+			else if (q == 0) { fi = pi / 2.0; }
+			else { fi = (atan(sqrt(-disk) / (-q / 2.0))) + pi; }
 
-		x1.real(2 * (sqrt(-p / 3.0)) * cos(fi / 3.0) - (b / 3.0 * a));
-		x2.real(2 * (sqrt(-p / 3.0)) * cos((fi / 3.0) + (2.0 * pi / 3.0)) - (b / 3.0 * a));
-		x3.real(2 * (sqrt(-p / 3.0)) * cos((fi / 3.0) + (4.0 * pi / 3.0)) - (b / 3.0 * a));
-		cout << "3 real sol:" << x1.real() << ", " << x2.real() << ", " << x3.real() << endl;
+			x1.real(2 * (sqrt(-p / 3.0)) * cos(fi / 3.0) - (b / 3.0 * a));
+			x2.real(2 * (sqrt(-p / 3.0)) * cos((fi / 3.0) + (2.0 * pi / 3.0)) - (b / 3.0 * a));
+			x3.real(2 * (sqrt(-p / 3.0)) * cos((fi / 3.0) + (4.0 * pi / 3.0)) - (b / 3.0 * a));
+			cout << "x1= " << x1.real() << "\nx2= " << x2.real() << "\nx3= " << x3.real() << endl;
 
-	}
-	else if (disk == 0.0) {
-		cout << disk << "2 real" << endl;
-		x1.real( 2 * cbrt(-q / 2.0)  - (b / 3.0 * a));
-		x2.real( - cbrt(-q / 2.0) - (b / 3.0 * a));
-		cout << "2 real sol:" << x1.real() << ", " << x2.real() << endl;
+		}
+		else if (disk == 0.0) {
+			cout << "Two real solutions: " << endl;
+			x1.real( 2 * cbrt(-q / 2.0)  - (b / 3.0 * a));
+			x2.real( - cbrt(-q / 2.0) - (b / 3.0 * a));
+			cout << "x1= " << x1.real() << "\nx2=  " << x2.real() << endl;
 
+		}
+		else {
+			cout << "Three solutions: " << endl;
+			double s = cbrt((-q / 2) + sqrt(disk));
+			double t = cbrt((-q / 2) - sqrt(disk));
+			x1.real( s + t - (b / 3 * a));
+			x2.real(-0.5 * (s + t) - (b / 3.0 * a));
+			x2.imag((sqrt(3.0) / 2.0) * (s - t));
+			complex<double> x3(conj(x2));
+			cout << "1 real solution: \n" << "x1 = " << x1.real() << "\n2 complex solutions: \n" << "x2 = " << x2.real() << " +(" << x2.imag() << ")i\n" << "x3 = " << x3.real() << " +(" << x3.imag() << ")i\n";
+
+		}
 	}
 	else {
-		cout << "2 compl" << endl;
-		
-		double s = cbrt((-q / 2) + sqrt(disk2));
-		double t = cbrt((-q / 2) - sqrt(disk2));
-		cout << "s" << s << endl << "t" << t << endl;
-		x1.real( s + t - (b / 3 * a));
-		x2.real(-0.5 * (s + t) - (b / 3.0 * a));
-		x2.imag((sqrt(3.0) / 2.0) * (s - t));
-		complex<double> x3(conj(x2));
-		cout << "1 real sol:" << x1.real() << ", 2 complex sol:" << "x2" << x2 << ", " << "x3" << x3 << endl;
-
+		cout << "Not a cubic equation. Try quadratic equation tool or check your coefficients\n";
 	}
-
 }
 static int GetAlphabeticOrderingNumber(char letter) {
 	if (letter >= 'a' && letter <= 'z') {
@@ -183,7 +189,7 @@ static void TriangleArea() {
 	else { cout << "Not a triangle. Try again with other points " << endl; }
 }
 static void ActionScene() {
-	cout << "FIRST PRACTICE\n1 - Quadratic equation\n2 - Triangle area\n3 - Distance between two points\n4 - Quadrangle area\n5 - Get letter's alphabet number from given word (english)\n ";
+	cout << "FIRST PRACTICE\n1 - Quadratic equation\n2 - Triangle area\n3 - Distance between two points\n4 - Quadrangle area\n5 - Get letter's alphabet number from given word (english)\n6 - Cubical equation\n ";
 	switch (ActionChooser())
 	{
 
@@ -211,7 +217,8 @@ static void ActionScene() {
 		break;
 	case 6:
 		//Решение кубического уравнения
-		cube_sol();
+		cout << "ax^3 + bx^2 + cx + d = 0\n";
+		CubicEquation();
 		break;
 	}
 }
