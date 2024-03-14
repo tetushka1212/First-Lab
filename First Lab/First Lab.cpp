2fea6c9947e2bfb72218de9fb76f5fd62047ae58
Yaroslav <iros04004@gmail.com>
2024-03-14 17:42:26 +0300

    Добавил рабочий второй сценарий для поиска площади треугольника

--- First Lab/First Lab.cpp
+++ First Lab/First Lab.cpp
@@ -73,8 +73,21 @@ void QuadraticEquation() {
 	
 	}
 }
+void TriangleArea() {
+	double area,x1,x2,x3,y1,y2,y3;
+	cout << "First point (x1,y1): " << endl;
+	cin >> x1 >> y1;
+	cout << "Second point (x2,y2): " << endl;
+	cin >> x2 >> y2;
+	cout << "Third point (x3,y3): " << endl;
+	cin >> x3 >> y3;
+
+	area = fabs(((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2.0);
+	if (area != 0) { cout << (area) << endl; }
+	else { cout << "Not a triangle. Try again with other points " << endl; }
+}
 void ActionScene() {
-	cout << "FIRST PRACTICE\n1 - Quadratic equation\n ";
+	cout << "FIRST PRACTICE\n1 - Quadratic equation\n2 - Triangle Area\n ";
 	switch (ActionChooser())
 	{
 
@@ -83,6 +96,10 @@ void ActionScene() {
 		//Решение квадратного уравнения
 		cout << "ax^2 + bx + c = 0\n";
 		QuadraticEquation();
+		break;
+	case 2:
+		//Площадь треугольника по координатам трех точек 
+		TriangleArea();
 		break;
 	}
 }
